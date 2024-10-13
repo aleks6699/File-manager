c1e11e438f5faf83ad0845e2abc86494de3e314a
Oleksandr <aleks6699@gmail.com>
2024-10-13 20:59:11 +0300

    feat: welcome to program start and shutdown

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+import process from "process";
+const userName = process.env.npm_config_username || "User";
+
+console.log(`Hello ${userName}!`);
+
+console.log(process.cwd());
+
+process.on("SIGINT", () => {
+  console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+  process.exit(0);
+});
+
+process.stdin.on("data", (data) => {
+  const input = data.toString().trim();
+  if (input === ".exit") {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+    process.exit(0);
+  }
+});
+
+
